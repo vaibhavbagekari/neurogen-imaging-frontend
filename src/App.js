a1de81fe32e97db68e090b5824728ebd567819af
Pujakare <pujakare@45gmail.com>
2024-09-26 22:20:37 +0530

    Initial commit of signup page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Components/Header';
+import Signup from './Components/Signup';
 
 function App() {
   return (
     <div className="App">
         <Header/>
+        <Signup></Signup>
     </div>
   );
 }
---
+++ src/Components/Signup.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Signup() {
+  return (
+    <div>
+      Signup form
+    </div>
+  )
+}
+
+export default Signup
