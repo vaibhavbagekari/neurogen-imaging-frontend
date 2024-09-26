03cdfaa5c8fa5269314479ebd92e333a6dfc06e4
Pratik Hake <pratikhake300@gmail.com>
2024-09-26 23:00:01 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Components/Header';
+import Hirosection from './Components/Hirosection';
 
 function App() {
   return (
     <div className="App">
         <Header/>
+        <Hirosection/>
     </div>
   );
 }
---
+++ src/Components/Hirosection.js
@@ -0,0 +1,10 @@
+const Hirosection =()=>{
+    return (
+        <>
+        <h1>
+            hii
+        </h1>
+        </>
+    );
+}
+export default Hirosection;
